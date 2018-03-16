6f5e546dc681c46a154e055bf03597d1b8bd498c
jessicawidjaja <36812014+jessicawidjaja@users.noreply.github.com>
2018-03-16 21:26:22 +0700

    Update Exercises 18.js

--- Jumat/Exercises 18.js
+++ Jumat/Exercises 18.js
@@ -1,15 +1,11 @@
 function kaliTerusRekursif(angka) {
-  // you can only write your code here!
   if (angka.toString().length === 1) {
   return angka;
   }
   else {
   var angka1 = Math.floor(angka / 10);
-  //console.log('hasil pembulatan bagi 10 ' + angka1);
   var angka2 = angka % 10;
-  //console.log('hasil modulus 10 ' + angka2);
   var hasilRekursif = kaliTerusRekursif(angka1);
-  //console.log('hasil kali ' + hasilRekursif);
   return kaliTerusRekursif(angka2 * hasilRekursif); 
   }
 }
