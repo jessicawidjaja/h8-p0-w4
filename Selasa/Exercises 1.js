e1fc321b3f09845d5ee1d26559b3fddb27c5248e
jessicawidjaja <36812014+jessicawidjaja@users.noreply.github.com>
2018-03-16 19:28:55 +0700

    Update Exercises 1.js

--- Selasa/Exercises 1.js
+++ Selasa/Exercises 1.js
@@ -1,10 +1,9 @@
 function angkaPrima(angka) {
-  // you can only write your code here!
   for(var i = 2; i < angka; i ++) {
-    if(angka%i === 0){
-      return false;
-    }
-  }
+  if(angka%i === 0){
+  return false;
+}
+}
   return true;
 }
 
