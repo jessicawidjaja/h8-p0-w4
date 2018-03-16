1422503fd427774a6ee7e24e498cafb69d0f731b
jessicawidjaja <36812014+jessicawidjaja@users.noreply.github.com>
2018-03-16 19:29:55 +0700

    Update Exercises 2.js

--- Selasa/Exercises 2.js
+++ Selasa/Exercises 2.js
@@ -1,13 +1,8 @@
-function fpb(angka1, angka2) {
-  
+function fpb(angka1, angka2) { 
   var hasil = 0;
-  
   for(var i = 1; i < angka1; i ++){
-
-    if(angka1 % i === 0 && angka2 % i === 0) { 
-      hasil = i;
-    }
-  }
+  if(angka1 % i === 0 && angka2 % i === 0) { hasil = i;}
+}
   return hasil;
   
 }
