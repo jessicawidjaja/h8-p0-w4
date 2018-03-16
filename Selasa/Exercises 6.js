ef97d608a2380685084faa37ba60de4ea2535ec7
jessicawidjaja <36812014+jessicawidjaja@users.noreply.github.com>
2018-03-16 21:38:39 +0700

    Update Exercises 6.js

--- Selasa/Exercises 6.js
+++ Selasa/Exercises 6.js
@@ -1,27 +1,31 @@
 function hitungHuruf(kata) {
-    var str = kata.toLowerCase();
-    var arr = kata.split(' ');
-    var count = 0;
-    var word;
-  
- for(var i =0; i<arr.length; i++){
- for(var a = 0; a < arr[i].length; a++){
-     var countNew = 0;
-   
- for(var b =0; b<arr[i].length;b++){
-      if(arr[i][a] === arr[i][b]){
-        countNew ++;
-      if(countNew>count){
-            count = countNew;
-            word = arr[i]; 
-          }
-        }
-      }
-    }
+  var pisah = kata.split(' ');
+  var kataPerulangan = []; 
+  var ulangHuruf = [];
+  for(var i = 0; i < pisah.length; i++){
+    var pisahHuruf = pisah[i].split(''); 
+    var hurufPerulangan = 0; 
+    for(var j = 0; j < pisah[i].length -1; j++){
+    for(var k = j; k < pisahHuruf.length; k++){
+        
+    if(j !== k && pisah[i][j] === pisahHuruf[k]){
+          hurufPerulangan += 1;
+    var splice = pisahHuruf.splice(k, 1);
+          
+ }
+ }
+ }
+    kataPerulangan.push(hurufPerulangan);
   }
-  return word;
+  
+  var perulanganTerbesar = Math.max.apply(null, kataPerulangan); 
+  var lokasiPerulanganTerbesar = kataPerulangan.indexOf(perulanganTerbesar);
+  if(lokasiPerulanganTerbesar !== 0){
+    return pisah[lokasiPerulanganTerbesar];
+  }else{
+    return -1;
+  }
 }
-
 // TEST CASES
 console.log(hitungHuruf('Today, is the greatest day ever')); // greatest
 console.log(hitungHuruf('I am a passionate developer')); // passionate
