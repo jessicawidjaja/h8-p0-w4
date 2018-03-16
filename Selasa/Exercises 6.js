70e37657dfed3290ba1a3e47b45e131a6fc253c2
jessicawidjaja <36812014+jessicawidjaja@users.noreply.github.com>
2018-03-16 19:52:10 +0700

    Update Exercises 6.js

--- Selasa/Exercises 6.js
+++ Selasa/Exercises 6.js
@@ -1,15 +1,17 @@
 function hitungHuruf(kata) {
-  var str = kata.toLowerCase();
-  var arr = kata.split(' ');
-  var count = 0;
-  var word;
-  for(var i =0; i<arr.length; i++){
-    for(var a = 0; a < arr[i].length; a++){
-      var countNew = 0;
-      for(var b =0; b<arr[i].length;b++){
-        if(arr[i][a] === arr[i][b]){
-          countNew ++;
-          if(countNew>count){
+    var str = kata.toLowerCase();
+    var arr = kata.split(' ');
+    var count = 0;
+    var word;
+  
+ for(var i =0; i<arr.length; i++){
+ for(var a = 0; a < arr[i].length; a++){
+     var countNew = 0;
+   
+ for(var b =0; b<arr[i].length;b++){
+      if(arr[i][a] === arr[i][b]){
+        countNew ++;
+      if(countNew>count){
             count = countNew;
             word = arr[i]; 
           }
